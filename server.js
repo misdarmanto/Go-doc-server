285af7afa6551aa7cdb49238df36c8f0073a1b11
Misdar <50019528+misdarmanto@users.noreply.github.com>
2022-07-19 07:36:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const PORT = process.env.PORT || 3001;
 const URI = false ? "https://go-docdoc.web.app" : "http://localhost:3000";
 const io = require("socket.io")(PORT, {
   cors: {
-    origin: "https://go-docdoc.web.app",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
+
 
 io.on("connection", (socket) => {
   socket.on("get-document", async (documentId) => {
