7655d69e668148a1ca49abd1db22886545a139e0
Misdar <50019528+misdarmanto@users.noreply.github.com>
2022-07-20 22:24:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const PORT = process.env.PORT || 3001;
 const URI = false ? "https://go-docdoc.web.app" : "http://localhost:3000";
-const io = require("socket.io")(PORT, {
-  cors: {origin: "*"},
-});
 
+const io = require("socket.io")(PORT, {
+ origins: ["https://go-docdoc.web.app"],
+  handlePreflightRequest: (req, res) => {
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "https://go-docdoc.web.app",
+      "Access-Control-Allow-Methods": "GET,POST",
+      "Access-Control-Allow-Headers": "my-custom-header",
+      "Access-Control-Allow-Credentials": true
+    });
+    res.end();
+  }
+});
 
 io.on("connection", (socket) => {
   socket.on("get-document", async (documentId) => {
